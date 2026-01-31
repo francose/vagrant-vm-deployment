600b29ea713c934ca6eee7fac3fc3e8c291aa6c3
francose <elmanoukdesign@gmail.com>
2026-01-31 17:20:58 -0500

    lab updated

--- README.md
+++ README.md
@@ -23,6 +23,29 @@ Test-Path "C:\HashiCorp\Vagrant\bin"
 vagrant --version
 ```
 
+## Run & Deployment
 
+```ps1
+vagrant up --provider vmware_desktop
 
+```
 
+### Next steps?
+
+* Downloading: Vagrant will download the Kali and Ubuntu "boxes" (images). This might take a few minutes depending on your internet.
+
+* Importing: It will import them into VMware Workstation.
+
+* Booting: You will see the VMware windows pop up and start booting the OS.
+
+* Ready: Once the terminal returns to the prompt, your lab is live!
+
+### How to use it:
+
+* To log into Kali: Just use the VMware window that popped up (the default credentials for the Kali box are usually kali / kali).
+
+* To check connectivity: From your Kali terminal, try to ping your target: ping 192.168.50.20.
+
+* To shut it down: Run vagrant halt.
+
+* To delete it all (start fresh): Run vagrant destroy.
--- lab.rb
+++ lab.rb
@@ -1,23 +1,29 @@
 Vagrant.configure("2") do |config|
   
-  # Set the default provider to VMware
+  # Tell Vagrant we are using VMware
   config.vm.provider "vmware_desktop" do |v|
-    v.gui = true 
-    v.linked_clone = true 
+    v.gui = true      # Shows the VM window so you can see the Kali desktop
+    v.memory = 2048   # 2GB RAM
+    v.cpus = 2
   end
 
-  # --- ATTACKER: ---
-#   config.vm.define "kali" do |kali|
-#     kali.vm.box = "kalilinux/kali-rolling"
-#     kali.vm.network "private_network", ip: "192.168.10.10"
-#   end
-
-  # --- TARGET:
-  config.vm.define "metasploitable" do |ms|
-    # Note: Metasploitable is often distributed as a VMX/VMDK. 
-    # This box is a community-maintained version for Vagrant.
-    ms.vm.box = "tleemcjr/metasploitable2"
-    ms.vm.network "private_network", ip: "192.168.10.20"
+  # --- THE ATTACKER: Kali Linux ---
+  config.vm.define "kali" do |kali|
+    kali.vm.box = "kalilinux/kali-rolling"
+    # Sets a static IP so you always know where your attacker is
+    kali.vm.network "private_network", ip: "192.168.50.10"
   end
 
+  # --- THE TARGET: Vulnerable Ubuntu ---
+  config.vm.define "target" do |target|
+    target.vm.box = "bento/ubuntu-22.04"
+    target.vm.network "private_network", ip: "192.168.50.20"
+    
+    # This "provisions" the target by installing a vulnerable app automatically
+    target.vm.provision "shell", inline: <<-SHELL
+      apt-get update
+      apt-get install -y docker.io
+      docker run -d -p 80:80 --name dvwa vulnerables/web-dvwa
+    SHELL
+  end
 end
